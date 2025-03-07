7bf3df03d917d10ce8e64becabd0af746fea8f0c
YJ <ypark0861@conestogac.on.ca>
2025-03-07 18:38:45 -0500

    Add typescript to the project

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,28 +1,27 @@
 // https://www.youtube.com/watch?v=aZGzwEjZrXc
+// https://flowbite.com/docs/components/navbar/
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
 
 const Appbar = () => {
     return (
-    <section className="nav-bar text-slate-950">
-      <nav className="px-3 sm:px-4">
-        <div className="container mx-auto flex flex-wrap items-center justify-btween sm:items-stretch sm:justify-start">
-          <div className="flex items-center cursor-pointer">
-            <img className="mr-3 h5 sm:h-6" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="logo" />
-            <h2>HEALTHY CHOICES</h2>
-          </div>
-          <div className="inline-flex items-center hidden sm:ml-6 sm:block">
-            <div className="flex space-x-4">
-              <Link to="/">
-              <div className="rounded-md bg-teal-400 px-3 py-2 text-sm font-medium text-slate-950 hover:bg-slate-300 hover:text-white" aria-current="page">Home</div>
-              </Link>
-              <Link to="/myfood">
-              <div className="rounded-md bg-teal-400 px-3 py-2 text-sm font-medium text-slate-950 hover:bg-slate-300 hover:text-white">My Food</div>
-              </Link>
-            </div>
-          </div>
+    <section>
+      <nav className="navbar">
+        <h1>HEALTHY CHOICES</h1>
+        <div className="links">
+          <ul className="flex">
+            <Link to="/" aria-current="page">
+            <li>
+              Home
+            </li>
+            </Link>
+            <Link to="/myfood">
+            <li>
+              My Food
+            </li>
+            </Link>
+          </ul>
         </div>
       </nav>
     </section>
@@ -30,3 +29,10 @@ const Appbar = () => {
 }
 
 export default Appbar;
+
+// <Link to="/">
+//               <div className="rounded-md bg-teal-400 px-3 py-2 text-sm font-medium text-slate-950 hover:bg-slate-300 hover:text-white" aria-current="page">Home</div>
+//               </Link>
+//               <Link to="/myfood">
+//               <div className="rounded-md bg-teal-400 px-3 py-2 text-sm font-medium text-slate-950 hover:bg-slate-300 hover:text-white">My Food</div>
+//               </Link>
--- src/pages/MyFood.jsx
+++ src/pages/MyFood.jsx
@@ -1,20 +1,38 @@
+// https://react.dev/reference/react-dom/components/input
+
 import{ useState, React } from 'react'
 import Button from 'react-bootstrap/Button';
 
 const MyFood = () => {
-  useState()
+  const [menuitem, setMenuitem] = useState('');
+  const [itemqty, setItemqty] = useState('0');
+  const qtyAsNumber = Number(itemqty);
 
   return (
     <div className="text-slate-950">
       <h1>My Food Checker</h1>
         <form>
-          <div>
-            <label htmlFor='menu' className="block text-sm/6 font-medium">Select a Menu:</label>
-            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-              <input type="text" placeholder="salad" id="menu" />
+          <div className="food-input">
+            <label htmlFor='menu' className="block text-sm/6 font-medium">Menu item:
+            <div className="flex items-center rounded-md bg-white pl-3 ">
+              <input 
+              value={menuitem}
+              onChange={e => setMenuitem(e.target.value)}
+              />
             </div>
+            </label>
+            <label>
+        Quantity:
+        <input
+          value={itemqty}
+          onChange={e => setItemqty(e.target.value)}
+          type="number"
+        />
+        <button>
+          Add
+        </button>
+      </label>
           </div>
-          <Button type="submit">Button</Button>
         </form>
         
         <h1 className="menu-list">Menu List</h1>
