bb1be9cdbe128a9f1c0f6ed2b821a6e12409e1f6
YJ <ypark0861@conestogac.on.ca>
2025-03-07 22:27:05 -0500

    add redux toolkit, card css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
--- src/pages/MyFood.jsx
+++ src/pages/MyFood.jsx
@@ -2,37 +2,45 @@
 
 import{ useState, React } from 'react'
 import Button from 'react-bootstrap/Button';
+import Counter from '../features/counter/Counter'
+import FoodCard from '../components/FoodCard';
 
 const MyFood = () => {
   const [menuitem, setMenuitem] = useState('');
   const [itemqty, setItemqty] = useState('0');
   const qtyAsNumber = Number(itemqty);
+  
+  function getFoodData() {
+    // send api request
+    // store response and item and qty to the store
+    
+
+  }
 
   return (
     <div className="text-slate-950">
       <h1>My Food Checker</h1>
-        <form>
+        <form action={getFoodData}>
           <div className="food-input">
             <label htmlFor='menu' className="block text-sm/6 font-medium">Menu item:
-            <div className="flex items-center rounded-md bg-white pl-3 ">
-              <input 
-              value={menuitem}
-              onChange={e => setMenuitem(e.target.value)}
-              />
-            </div>
+              <div className="flex items-center rounded-md bg-white pl-3 ">
+                <input 
+                value={menuitem}
+                onChange={e => setMenuitem(e.target.value)}
+                />
+              </div>
             </label>
-            <label>
-        Quantity:
-        <input
-          value={itemqty}
-          onChange={e => setItemqty(e.target.value)}
-          type="number"
-        />
-        <button>
-          Add
-        </button>
-      </label>
+            <label> Quantity:
+              <input
+                value={itemqty}
+                onChange={e => setItemqty(e.target.value)}
+                type="number"
+              />
+            </label>
           </div>
+          <button type="submit">
+                Add
+          </button>
         </form>
         
         <h1 className="menu-list">Menu List</h1>
@@ -40,6 +48,8 @@ const MyFood = () => {
           <li>1</li>
           <button>Delete</button>
         </ul>
+        <Counter />
+        <FoodCard />
     </div>
   )
 }
