df068ed8b7f7f86007353462a89139a1031aeb5d
Elijah <94766721+joagBov@users.noreply.github.com>
2025-04-14 20:59:11 -0400

    Update Home.tsx

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -3,16 +3,22 @@
 // PROGRAMMER: Yujung Park
 // FIRST VERSION: 2025-03-05
 // DESCRIPTION: 
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-slate-950">
-        Home page 
-      </h1>
+    <div className="home">
+      <div className="flex-grow h-1"></div>
+      <section className="welcome-section">
+        <h1>Welcome to Healthy Choices!</h1>
+        <p>
+          We are here to help track your meals, find restaurants, and make better, healthier food choices.
+        </p>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
